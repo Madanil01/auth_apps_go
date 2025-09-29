276d6acc6ca7c03e3354f3a4b22e12e19dbf66c8
madanil <madanildanil01@gmail.com>
2025-09-29 14:02:26 +0700

    [UPD] update

--- api/main.go
+++ api/main.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-		"github.com/gofiber/fiber/v2";
-		"apps_v1/database";
-		"apps_v1/routes";
-		"github.com/joho/godotenv"
-		"github.com/gofiber/fiber/v2/middleware/cors";
-		"log"
-		"os"
-		
-	)
+	"github.com/gofiber/fiber/v2"
+	"apps_v1/database"
+	"apps_v1/routes"
+	"github.com/joho/godotenv"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"log"
+	"os"
+)
 
+var app *fiber.App
 
-
-func main() {
+func init() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
+
 	var ALLOW_ORIGIN string = os.Getenv("ALLOW_ORIGIN")
 	database.ConnectDB()
-	app := fiber.New()
+
+	app = fiber.New()
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     ALLOW_ORIGIN,
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
@@ -28,11 +29,13 @@ func main() {
 		AllowCredentials: true,
 	}))
 	app.Static("/uploads", "./uploads")
-	// routes.WebSocketRoute(app)
+
+	// routes
 	routes.Setup(app)
 	routes.AuthRoute(app)
-
-	app.Listen("0.0.0.0:8002")
-	 
 }
 
+// ✅ Vercel expects an exported function
+func Handler() *fiber.App {
+	return app
+}
